ecc8e0bbf878ad8640dcc6732d3f01da57ac5440
Alex Igbokwe <juvitafrica@gmail.com>
2022-01-02 00:23:10 +0100

    feature: comment model

---
+++ App/DTO/Comment/IComment.ts
@@ -0,0 +1,7 @@
+interface IComment {
+  id?: number;
+  commented_by: number;
+  comment_content: string;
+}
+
+export default IComment;
--- App/Model/Comment_model.ts
+++ App/Model/Comment_model.ts
@@ -1,9 +1,12 @@
 "use strict";
-    import {Model} from "Elucidate/Database/Model";
-    class Comment extends Model{
-      // Table name
-      static tableName = "comment"
-      
-    }
+import IComment from "App/DTO/Comment/IComment";
+import { Model } from "Elucidate/Database/Model";
+class Comment extends Model implements IComment {
+  id!: number;
+  commented_by!: number;
+  comment_content!: string;
+  // Table name
+  static tableName = "comment";
+}
 
-    export default Comment;
+export default Comment;
