68bc423efe1faf4556180af7ec4f7833d9685b80
Alex Igbokwe <juvitafrica@gmail.com>
2022-01-02 09:38:02 +0100

    feature:comment route

--- Routes/Comment/index.ts
+++ Routes/Comment/index.ts
@@ -3,21 +3,13 @@ import Route from "Elucidate/Route/manager";
 
 /*
     |--------------------------------------------------------------------------
-    | Post Route File   
+    | Comment Route File   
     |--------------------------------------------------------------------------
     | 
     */
 
-Route.group({ prefix: "post", middleware: ["auth"] }, () => {
-  Route.get("/all", "PostController@index");
-
-  Route.get("/get/:post_id", "PostController@show");
-
-  Route.post("/save", "PostController@store");
-
-  Route.get("/like", "PostController@like");
-
-  Route.delete("/delete/:post_id", "PostController@destroy");
+Route.group({ prefix: "comment", middleware: ["auth"] }, () => {
+  Route.post("/save", "CommentController@store");
 });
 
 //--------------------------------------------------------------------------
